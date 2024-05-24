eed75236af877d435909a7eec8a878c77262a02b
Harrison Iruthayathas <hiruthayathas@myseneca.ca>
2024-05-24 14:17:43 -0400

    Lab 2 Submission

---
+++ lab2b.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#name = 'Jon'
+#age = 20
+#print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+name = input("Name: ")
+age = input("Age: ")
+#colour = input("Type in a colour and press enter: ")
+print('Hi '+name+', you are '+age+' years old.')
